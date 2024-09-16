59ba50c584510f85a263070659fab31e20f51578
mussaratshamsher <musaratskhan@github.com>
2024-09-15 22:39:01 -0700

    first

--- script.ts
+++ script.ts
@@ -24,3 +24,47 @@ document.getElementById('resume-form')?.addEventListener('submit', function(even
 
     document.getElementById('resume-content')!.innerHTML = resumeContent;  
 });
+
+//code 2
+
+// const resumeForm = document.getElementById('resume-form') as HTMLInputElement
+// const resumeOutput = document.getElementById('resume-content') as HTMLInputElement
+
+
+// if(resumeForm && resumeOutput){
+//     resumeForm.addEventListener('submit',(event)=>{
+//         event.preventDefault();
+
+//         const name = (document.getElementById('name')as HTMLInputElement).value;  
+//     const email = (document.getElementById('email')as HTMLInputElement).value;  
+//     const phone = (document.getElementById('phone')as HTMLInputElement).value;  
+//     const experience = (document.getElementById('experience')as HTMLInputElement).value;  
+//     const education = (document.getElementById('education')as HTMLInputElement).value;  
+//     const skills = (document.getElementById('skills')as HTMLInputElement).value;
+
+
+//     const resumeContent = ` 
+//             <h2>Personal Information</h2> 
+            
+//             <p>Name: ${name}</p>
+//             <p>Email: ${email}</p>  
+//             <p>Phone: ${phone}</p>  
+//             <h4>Experience</h4>  
+//             <p>${experience}</p>  
+//             <h4>Education</h4>  
+//             <p>${education}</p>  
+//              <h4>Skills</h4>
+//              <p>${skills}</p>
+//         `;  
+// resumeOutput.innerText = resumeContent
+
+
+//     })
+// }  else{
+//     console.error('output div is not found in DOM')
+// }
+
+
+
+
+
